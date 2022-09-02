b13df795a3ba9764e2d6b4b25e72d9b02526be09
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 22:29:19 +0530

    Add files via upload

---
+++ if_else.js
@@ -0,0 +1,9 @@
+const time =15;
+if (time<10)
+    console.log("good morning");
+else if (time<19)
+    console.log("good evening");
+else if (time<22)
+    console.log("good night");
+else
+    console.log("Invaild time");
