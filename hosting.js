9317398b9a383bd3a05f64c11a2db61d96048097
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 22:28:58 +0530

    Add files via upload

---
+++ hosting.js
@@ -0,0 +1,14 @@
+// console.log(test);   
+// var test;
+
+// var test;
+// console.log(test);
+
+// a = 5;
+// console.log(a);
+// var a;
+
+
+// let person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}; 
+// console.log(person)
+
