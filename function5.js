65177b317343c57845f33ed2b6c799c12f7bd70c
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 22:28:08 +0530

    Add files via upload

---
+++ function5.js
@@ -0,0 +1,5 @@
+function function_print_lines(num1,num2){
+    console.log(num1);
+    console.log(num2);
+  }
+  function_print_lines("Mai kanpur ki rhene vali hoo .", "aur mai ek student hoo navgurukul ki aur mai yha study krne aur job lene aayi hoo.")
