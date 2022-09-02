e8cf6cec93072114b0176c2b78d09a0a6167cd34
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 22:24:05 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,80 @@
+// var a=1;
+// var str="";
+// // var i=0
+// for (var i=1; i<=a; i++){
+//     for (const j="i"; j>=5; j++){
+//         str +=i;
+//     }
+//     str +="*";
+
+// }
+// console.log(str);
+
+// var i=1;
+// var str=""
+// while(i<=5){
+//     var j=0;
+//     while(j<i){
+//         str+="*"
+//         j=j+1;
+//     }
+//     str="\n"
+//     i=i+1
+// }
+// console.log(str)
+
+// var i=1;
+// var s=""
+// while(i<=5){
+//     var j=0;
+//     while(j<i){
+//         s+="*"
+//         j+=1;
+//     }
+//     s+="\n"
+//     i+=1
+// }
+// console.log(s)
+
+
+
+// let i=1;
+// let str=""
+// while(i>=5){
+//     let k=0;
+//     while(k>i){
+//         str+="*"
+//         k++;
+//     }
+//     str+"\n"
+//     i++
+// }
+// console.log(str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// a=[0,1,0,3,12,0]
+// var i=0;
+// while(i<a.length){
+//     a=[0,3,4,2,5,6]
+//     i++
+// }
+
+// console.log(a)
