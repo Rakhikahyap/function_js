786c1e77b353847d9f608d55de0236431bcc2d89
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 22:34:39 +0530

    Add files via upload

---
+++ list.js
@@ -0,0 +1,19 @@
+// const cars = ["Saab", "Volvo", "BMW"];
+// let car = cars[0];
+// console.log(cars)
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// cars[0] = "Opel";
+// console.log(cars);
+
+// const person = ["John", "Doe", 46];
+// console.log(person);
+
+// var a=4;
+// function my_func(){
+//     var a=6;
+//     console.log(a)
+// }
+// my_func()
+// console.log(a)
+
