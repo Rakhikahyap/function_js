822158fa615aa499a99a0d80f560fe6316126103
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 22:32:27 +0530

    Add files via upload

---
+++ opet.js
@@ -0,0 +1,21 @@
+// const x ='8';
+// const y =4;
+// const result=x+y;
+// console.log(result)
+// console.log(typeof(result))
+
+
+// let result;
+
+result = 4+undefined;
+console.log(result);
+
+// result =4-undefined;
+// console.log(result);
+
+
+// result = true+ undefined;
+// console.log(result);
+
+// result =null +undefined;
+// console.log(result);
